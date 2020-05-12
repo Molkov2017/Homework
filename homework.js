c9332bdc4dee4186fa301fff7731d99eb0e31c41
Molkov2017 <Molkov2017@yandex.ru>
2020-05-12 23:46:24 +0300

    4 lesson

--- homework.js
+++ homework.js
@@ -1,84 +1,20 @@
 'use strict';
-let money, time;
-
-function start() {
-    money = +prompt("Ваш бюджет на месяц?", '');
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
-    while (isNaN(money) || money == "" || money == null) {
-        money = +prompt("Ваш бюджет на месяц,", '');
+/* const options = {
+    name: 'text',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
     }
-}
-start();
-
-let appData = {
-    budget: money,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    timeData: time,
-    savings: true
 };
 
-function chooseExprensive() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-            b = prompt("Во сколько это обойдется?", "");
-        if ((typeof (a)) === "string" && (typeof (a)) != null && (typeof (b)) != null &&
-            a != " " && b != " " && a.length < 50) {
-            console.log("Данные введены правильно");
-            appData.expenses[a] = b;
-        } else {
-            console.log("Неверный формат введённых данных");
-            i--;
-
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key] [i]}`);
         }
-
-    }
-}
-chooseExprensive();
-
-function chooseOptExpensive() {
-    for (let i = 0; i < 3; i++) {
-        let questingOptExpenses = prompt("Введите необязательную статью расходов:");
-        if ((typeof (questingOptExpenses)) === "string" && (typeof (questingOptExpenses)) != null &&
-        questingOptExpenses != " " && questingOptExpenses.length < 50) {
-            appData.optionalExpenses[i] = questingOptExpenses;
-        } else {
-            console.log("Неверный формат введённых данных");
-            i--;
-        }
-    }
-}
-chooseOptExpensive();
-
-function detectDayBudget() {
-    appData.moneyPerDay = appData.budget / 30;
-    alert(`Ваш ежедневный бюджет равен: ${Math.round(appData.moneyPerDay)} рублей`);
-}
-detectDayBudget();
-
-function detectLevel() {
-    if (appData.moneyPerDay <= 1000) {
-        alert("К сожалению у Вас низкий уровень дохода");
-    } else if (appData.moneyPerDay > 1000 && appData.moneyPerDay < 2000) {
-        alert("Неплохо, у Вас средний уровень дохода");
-    } else if (appData.moneyPerDay > 2000) {
-        alert("Поздравляем, у Вас высокий уровень дохода");
     } else {
-        console.log("Критическая ситуация");
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
     }
-}
-detectLevel();
-
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt("Какое количество средств хранится на депозите?", ""),
-            percent = +prompt("Под какой процент хранятся средства?", "");
-        appData.monthIncome = save / 100 / 12 * percent;
-        alert("Ежемесячный доход с Вашего депозита составляет: " + Math.round(appData.monthIncome) +
-            " рублей.");
-    }
-}
-checkSavings();
-
-console.log(appData);
+} */
--- script.js
+++ script.js
@@ -1,43 +1,66 @@
 'use strict';
-/* Задание на урок:
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-// Код возьмите из предыдущего домашнего задания
-
-const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Какой фильм Вы посмотрели последним?", ""),
-        b = prompt("Оцените его по десятибальной шкале", "");
+        count: 0,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false,
+        start: function () {
+            personalMovieDB.count = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+            while (personalMovieDB.count == "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+                personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?', "");
+            }
+        },
+        answer: function () {
+            for (let i = 0; i < 2; i++) {
+                const a = prompt("Какой фильм Вы посмотрели последним?", ""),
+                    b = prompt("Оцените его по десятибальной шкале", "");
 
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
-    }
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено мало фильмов", "");
-    } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
-        console.log("Вы - классический зритель");
-    } else if (personalMovieDB.count > 30) {
-        console.log("Вы киноман");
-    }
-}
-console.log(personalMovieDB);
+                if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+                    personalMovieDB.movies[a] = b;
+                } else {
+                    i--;
+                }
+            }
+        },
+        userLevelMov: function () {
+            if (personalMovieDB.count < 10) {
+                console.log("Просмотрено мало фильмов", "");
+            } else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+                console.log("Вы - классический зритель");
+            } else if (personalMovieDB.count > 30) {
+                console.log("Вы киноман");
+            }
+        },
+        toggleVisibleMyDB: function () {
+            if (personalMovieDB.privat) {
+                personalMovieDB.privat = false;
+            } else {
+                personalMovieDB.privat = true;
+            }
+
+        },
+        showMyDB: function (hidden) {
+            if (!hidden) {
+                console.log(personalMovieDB);
+            }
+        },
+
+        writeYourGenres: function () {
+            for (let i = 1; i < 2; i++) {
+                let genres = prompt("Введите Ваши любимые жанры через запятую");
+                if (genres === "" || genres == null) {
+                    console.log("Вы ввели некорректные данные или не ввели их совсем");
+                    i--;
+                } else {
+                    personalMovieDB.genres = genres.split(", ");
+                    personalMovieDB.genres.sort();
+                }
+            }
+
+            personalMovieDB.genres.forEach((item, i) => {
+                    console.log(`Любимый жанр ${i + 1} - это ${item}`);
+                });
+            }
+};
